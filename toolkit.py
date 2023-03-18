4efeb779bc2348e450366d30e92420863c2253d8
natymeng <121955827+natymeng@users.noreply.github.com>
2023-03-18 23:28:48 +0000

    Add files via upload

--- toolkit.py
+++ toolkit.py
@@ -12,7 +12,7 @@ from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import mean_absolute_error as mae
 from sklearn.metrics import mean_absolute_percentage_error as mape
 
-def load_data(ticker):
+def load_data():
     stocks = pd.read_csv('stock.csv')
     stocks['Date'] = pd.to_datetime(stocks['Date'])
     stocks['Date'] = stocks['Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
@@ -24,18 +24,18 @@ def load_data(ticker):
     stocks = stocks.merge(fx, how = 'left', on = 'Date')
     stocks['Price USD'] = np.where(stocks['Country'] == 'JP', stocks['Price']/stocks['JP/US'], 
                               np.where(stocks['Country'] == 'UK', stocks['Price'] * stocks['US/UK'], stocks['Price']))
-    
-    df = stocks[stocks['ID'] == ticker][['Date','Price USD','Sentiment']]
-    df.fillna(method = 'ffill', inplace = True) #in case there's no price, fill with previous day close
+    return stocks
+
+def stock_df(stocks, ticker):
+    df = stocks[stocks['ID'] == ticker][['Date','Country','Volume','Volatility','Price USD','Sentiment','Spread']]
+    df.fillna(method = 'ffill', inplace = True) #in case there's no data point, fill with previous day data point
     return df
 
-# def normalize(data):
-#     """
-#     Takes a Series and normalize it.
-#     """
-#     scaler = MinMaxScaler(feature_range=(0,1)) 
-#     scaler.fit(np.concatenate((series1.reshape(-1, 1), series2.reshape(-1, 1)), axis=1))
-#     return norm_series
+def normalized_data(data_series, feature_low=0, feature_high=1):
+    scaler = MinMaxScaler(feature_range=(feature_low,feature_high)) 
+    scaler.fit(data_series.values.reshape(-1, 1))
+    data_norm = scaler.transform(data_series.values.reshape(-1, 1))
+    return data_norm
 
 def df_to_XY(px_norm, window_size=5):
     """
@@ -72,68 +72,59 @@ def performance_measure(model, X, y, method = 'Standard LSTM'):
                          'MAPE': mape(y, predictions), 
                          'Accuracy': 1-mape(y, predictions)}, index = [method])
 
-def standard_LSTM(df, window_size = 5, train_split = 0.8, method = 'Standard LSTM'):
+def LSTM_layers(input_row, input_col):
+    """
+    Takes an input row number and an column number to set up the layers in the various versions of LSTM models.
+    """
+    model0 = Sequential()
+    model0.add(LSTM(units=100,return_sequences=True,input_shape=(input_row, input_col)))
+    model0.add(LSTM(units=100,return_sequences=True))
+    model0.add(Dropout(0.1))
+    model0.add(LSTM(units=100,return_sequences=True))
+    model0.add(Dropout(0.1))
+    model0.add(LSTM(units=100))
+    model0.add(Dropout(0.1))
+    model0.add(Dense(units=1))
+    return model0
+
+def standard_LSTM(data_series, feature_low = 0, feature_high = 1, window_size = 5, train_split = 0.8, method = 'Standard LSTM'):
     """
     This function sets up the standard LSTM model.
     This model has 3 LSTM layer with 100 units per layer, followed by a dropout layer with dropout rate of 0.1.
     The input later takes the input shape of nx1, where n is the size of the rolling window.
     """
-    scaler = MinMaxScaler(feature_range=(0,1)) 
-    scaler.fit(df['Price USD'].values.reshape(-1, 1))
-    
-    px_norm = scaler.transform(df['Price USD'].values.reshape(-1, 1))
+    px_norm = normalized_data(data_series, feature_low, feature_high)
     X0, Y0 = df_to_XY(px_norm, window_size)
     X0_train, Y0_train, X0_test, Y0_test = split_data(X0, Y0, train_split)
 
-    model0 = Sequential()
-    model0.add(LSTM(units=100,return_sequences=True,input_shape=(window_size, 1))) 
-    model0.add(LSTM(units=100,return_sequences=True))
-    model0.add(Dropout(0.1))
-    model0.add(LSTM(units=100,return_sequences=True))
-    model0.add(Dropout(0.1))
-    model0.add(LSTM(units=100))
-    model0.add(Dropout(0.1))
-    model0.add(Dense(units=1))
-
+    model0= LSTM_layers(window_size, 1)
     model0.compile(loss=MeanSquaredError(), optimizer=Adam(learning_rate=0.001))
     model0.fit(X0_train,Y0_train, epochs=10, verbose = 0)
 
     res0 = performance_measure(model0, X0_test, Y0_test.flatten(), method)
 
-    return res0
+    return model0, res0
 
-def LSTM_pair(df, window_size = 5, train_split = 0.8, method = 'Standard LSTM'):
+def LSTM_pair(data_series1, data_series2, feature_low1 = 0, feature_high1 = 1, feature_low2 = 0, feature_high2 = 1, 
+              window_size = 5, train_split = 0.8, method = 'Standard LSTM'):
     """
     This function sets up the LSTM model with historical price data and sentiment score data as a pair.
     This model has 3 LSTM layer with 100 units per layer, followed by a dropout layer with dropout rate of 0.1.
     The input later takes the input shape of nx2, where n is the size of the rolling window.
     """
-    scaler = MinMaxScaler(feature_range=(0,1)) 
-    scaler.fit(np.concatenate((df['Price USD'].values.reshape(-1, 1), df['Sentiment'].values.reshape(-1, 1)), axis=1))
-    
-    px_norm = scaler.transform(df['Price USD'].values.reshape(-1, 1))
-    sent_norm = scaler.transform(df['Sentiment'].values.reshape(-1, 1))
+    px_norm = normalized_data(data_series1, feature_low1, feature_high1)
+    sent_norm = normalized_data(data_series2, feature_low2, feature_high2)
     df_norm = np.hstack((px_norm[1:], sent_norm[:-1]))
-    
     
     X0, Y0 = df_to_XY(df_norm, window_size)
     X0_train, Y0_train, X0_test, Y0_test = split_data(X0, Y0, train_split)
 
-    model0 = Sequential()
-    model0.add(LSTM(units=100,return_sequences=True,input_shape=(window_size, 2))) 
-    model0.add(LSTM(units=100,return_sequences=True))
-    model0.add(Dropout(0.1))
-    model0.add(LSTM(units=100,return_sequences=True))
-    model0.add(Dropout(0.1))
-    model0.add(LSTM(units=100))
-    model0.add(Dropout(0.1))
-    model0.add(Dense(units=1))
-
+    model0= LSTM_layers(window_size, 2)
     model0.compile(loss=MeanSquaredError(), optimizer=Adam(learning_rate=0.001))
     model0.fit(X0_train,Y0_train, epochs=10, verbose = 0)
 
     res1 = performance_measure(model0, X0_test, Y0_test.flatten(), method)
-    return res1
+    return model0, res1
 
 def df_to_XY_add_sent(px_norm, sent_norm, window_size=5):
     X = []
@@ -143,42 +134,40 @@ def df_to_XY_add_sent(px_norm, sent_norm, window_size=5):
         Y.append(px_norm[i+window_size, 0])
     return np.array(X), np.array(Y)
 
-def LSTM_addl_feature(df, window_size = 5, train_split = 0.8, method = 'Standard LSTM'):
+def LSTM_addl_feature(data_series1, data_series2, feature_low1 = 0, feature_high1 = 1, feature_low2 = 0, feature_high2 = 1, 
+                      window_size = 5, train_split = 0.8, method = 'Standard LSTM'):
     """
     This function sets up the LSTM model with historical price data and sentiment score data as an additional feature on the input.
     This model has 3 LSTM layer with 100 units per layer, followed by a dropout layer with dropout rate of 0.1.
     The input later takes the input shape of (n+1)x1, where n is the size of the rolling window.
     """
-    scaler = MinMaxScaler(feature_range=(0,1)) 
-    scaler.fit(np.concatenate((df['Price USD'].values.reshape(-1, 1), df['Sentiment'].values.reshape(-1, 1)), axis=1))
-    
-    px_norm = scaler.transform(df['Price USD'].values.reshape(-1, 1))
-    sent_norm = scaler.transform(df['Sentiment'].values.reshape(-1, 1))
+    px_norm = normalized_data(data_series1, feature_low1, feature_high1)
+    sent_norm = normalized_data(data_series2, feature_low2, feature_high2)
+
     df_norm = np.hstack((px_norm[1:], sent_norm[:-1]))
-    
     
     X0, Y0 = df_to_XY_add_sent(px_norm, sent_norm, window_size)
     X0_train, Y0_train, X0_test, Y0_test = split_data(X0, Y0, train_split)
-
-    model0 = Sequential()
-    model0.add(LSTM(units=100,return_sequences=True,input_shape=(window_size+1, 1)))
-    model0.add(LSTM(units=100,return_sequences=True))
-    model0.add(Dropout(0.1))
-    model0.add(LSTM(units=100,return_sequences=True))
-    model0.add(Dropout(0.1))
-    model0.add(LSTM(units=100))
-    model0.add(Dropout(0.1))
-    model0.add(Dense(units=1))
-
+    
+    model0= LSTM_layers(window_size+1, 1)
     model0.compile(loss=MeanSquaredError(), optimizer=Adam(learning_rate=0.001))
     model0.fit(X0_train,Y0_train, epochs=10, verbose = 0)
 
     res2 = performance_measure(model0, X0_test, Y0_test.flatten(), method)    
-    return res2
+    return model0, res2
 
 
-def compare(res0, res1, res2):
-    return pd.concat([res0, res1, res2], axis = 0)
+# def compare(res0, res1, res2):
+#     return pd.concat([res0, res1, res2], axis = 0)
+
+def run_price_predict(ticker):
+    df_stock = stock_df(load_data(), ticker)
+    df1 = df_stock['Price USD']
+    df2 =  df_stock['Sentiment']
+    summary = pd.concat([standard_LSTM(df1)[1], 
+                     LSTM_pair(df1,df2, method = 'LSTM Pair')[1],
+                     LSTM_addl_feature(df1, df2, method = 'LSTM Addl feature')[1]])
+    return summary
 
 
 def predict_LSTM_af(df, window_size = 5, train_split = 0.8):
@@ -197,16 +186,7 @@ def predict_LSTM_af(df, window_size = 5, train_split = 0.8):
     X0, Y0 = df_to_XY_add_sent(px_norm, sent_norm, window_size)
     X0_train, Y0_train, X0_test, Y0_test = split_data(X0, Y0, train_split)
 
-    model0 = Sequential()
-    model0.add(LSTM(units=100,return_sequences=True,input_shape=(window_size+1, 1)))
-    model0.add(LSTM(units=100,return_sequences=True))
-    model0.add(Dropout(0.1))
-    model0.add(LSTM(units=100,return_sequences=True))
-    model0.add(Dropout(0.1))
-    model0.add(LSTM(units=100))
-    model0.add(Dropout(0.1))
-    model0.add(Dense(units=1))
-
+    model0= LSTM_layers(window_size+1, 1)
     model0.compile(loss=MeanSquaredError(), optimizer=Adam(learning_rate=0.001))
     model0.fit(X0_train,Y0_train, epochs=10, verbose = 0)
 
@@ -214,4 +194,27 @@ def predict_LSTM_af(df, window_size = 5, train_split = 0.8):
     oneday_predict_px = scaler.inverse_transform(oneday_predict)
     accuracy = performance_measure(model0, X0_test, Y0_test.flatten())['Accuracy']
 
-    return [oneday_predict_px[0][0], accuracy[0]]
+    return oneday_predict_px[0][0], accuracy[0]
+
+    
+def predivt_LSTM_volume(ticker, window_size = 5, train_split = 0.8):
+    df = stock_df(load_data(), ticker).set_index('Date')
+    
+    scaler = MinMaxScaler(feature_range=(0,1))
+    scaler.fit(df['Volume'].values.reshape(-1, 1))
+    vol_norm = scaler.transform(df['Volume'].values.reshape(-1, 1))
+
+    window_size = 5
+    train_split = 0.8
+
+    X, Y = df_to_XY(vol_norm, window_size=5)
+    X_train, Y_train, X_test, Y_test = split_data(X, Y, train_split)
+
+    model3 = LSTM_layers(window_size, 1)
+    model3.compile(loss=MeanSquaredError(), optimizer=Adam(learning_rate=0.001))
+    model3.fit(X_train,Y_train, epochs=5, verbose = 0)
+
+    oneday_predict = model3.predict(vol_norm[-window_size:].reshape(1, window_size, 1))
+    oneday_predict_volume = scaler.inverse_transform(oneday_predict)
+
+    return oneday_predict_volume
